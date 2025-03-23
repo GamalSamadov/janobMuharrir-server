01ffbf6552eb1cf056062b74947d95caa4a6e2ac
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-03-23 16:42:19 +0300

    FIX: vercel deploy 29

--- src/server.ts
+++ src/server.ts
@@ -20,44 +20,45 @@ export const prisma = new PrismaClient()
 
 const app = express()
 
-app.use(express.json())
-app.use(cookieParser())
-
-app.use(
-	cors({
-		origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
-		credentials: true,
-		exposedHeaders: ['Set-Cookie']
-	})
-)
-
-app.get('/test', (req: Request, res: Response) => {
-	res.json({ message: 'Hello World' }).status(200)
-})
-app.use('/api/auth', authController)
-app.use('/api/users', userController)
-app.use('/api/sessions', sessionController)
-app.use('/api/events', eventController)
-app.use('/api/transcripts', transcriptController)
-
-app.all('*', (req: Request, res: Response) => {
-	res.status(404).json({ message: `Route ${req.originalUrl} Not Found` })
-})
-
 async function run() {
-	try {
+	app.use(express.json())
+	app.use(cookieParser())
+	app.use(
+		cors({
+			origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
+			credentials: true,
+			exposedHeaders: ['Set-Cookie']
+		})
+	)
+
+	app.get('/test', (req: Request, res: Response) => {
+		res.json({ message: 'Hello World' }).status(200)
+	})
+	app.use('/api/auth', authController)
+	app.use('/api/users', userController)
+	app.use('/api/sessions', sessionController)
+	app.use('/api/events', eventController)
+	app.use('/api/transcripts', transcriptController)
+
+	app.all('*', (req: Request, res: Response) => {
+		res.status(404).json({
+			message: `Route ${req.originalUrl} Not Found`
+		})
+	})
+}
+
+run()
+	.then(async () => {
 		await prisma.$connect()
 		logger.info('Connected to database')
 		const port = process.env.PORT || 4200
 		app.listen(port, () => {
 			logger.info(`Server is running on: http://localhost:${port}`)
 		})
-	} catch (e) {
+	})
+	.catch(e => {
 		logger.error(`Failed to connect to database ${e}`)
 		process.exit(1)
-	}
-}
-
-run()
+	})
 
 export default app
