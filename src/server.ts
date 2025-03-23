9da7d04b3bb85361f2bb523282aec620b47af46c
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-03-23 12:07:01 +0300

    FIX: deploy on vercel cors 3

--- src/server.ts
+++ src/server.ts
@@ -28,13 +28,6 @@ async function main() {
 		cors({
 			origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
 			credentials: true,
-			methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-			allowedHeaders: [
-				'Content-Type',
-				'Authorization',
-				'X-Requested-With',
-				'Accept'
-			],
 			exposedHeaders: ['Set-Cookie']
 		})
 	)
