09e073b753da57442bafaef6fae4e1d15251dff0
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-03-23 16:05:59 +0300

    FIX: vercel deploy 28

--- src/utils/download-audio.ts
+++ src/utils/download-audio.ts
@@ -1,13 +1,13 @@
 import ytdl from '@distube/ytdl-core'
-import ffmpegInstaller from '@ffmpeg-installer/ffmpeg'
-import ffprobeInstaller from '@ffprobe-installer/ffprobe'
+// import ffmpegInstaller from '@ffmpeg-installer/ffmpeg'
+// import ffprobeInstaller from '@ffprobe-installer/ffprobe'
 import ffmpeg from 'fluent-ffmpeg'
 import fs from 'fs'
 import path from 'path'
 import { v4 as uuidv4 } from 'uuid'
 
-ffmpeg.setFfmpegPath(ffmpegInstaller.path)
-ffmpeg.setFfprobePath(ffprobeInstaller.path)
+// ffmpeg.setFfmpegPath(ffmpegInstaller.path)
+// ffmpeg.setFfprobePath(ffprobeInstaller.path)
 
 export async function downloadYoutubeAudio(
 	url: string,
