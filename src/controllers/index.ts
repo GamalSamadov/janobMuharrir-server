debb06909dc3691de0ea9e2c805a3baeac4b779e
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-03-23 18:32:38 +0300

    FIX: vercel deploy 33

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,5 @@
 export { default as authController } from './auth/auth.controller'
-export { default as eventController } from './user/user.controller'
+export { default as userController } from './user/user.controller'
 export { default as sessionController } from './session/session.controller'
 export { default as transcriptController } from './transcript/transcript.controller'
-export { default as userController } from './event/event.controller'
+export { default as eventController } from './event/event.controller'
