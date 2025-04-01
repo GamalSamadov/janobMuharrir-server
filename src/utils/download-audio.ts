73da78140b553cae0e1fa1e6eb155e41fa7df0d0
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-04-01 12:25:39 +0300

    FIX: downloading on production issue 4/4

--- src/utils/download-audio.ts
+++ src/utils/download-audio.ts
@@ -1,10 +1,10 @@
-import ytdl from '@distube/ytdl-core'
 // import ffmpegInstaller from '@ffmpeg-installer/ffmpeg'
 // import ffprobeInstaller from '@ffprobe-installer/ffprobe'
 import ffmpeg from 'fluent-ffmpeg'
 import fs from 'fs'
 import path from 'path'
 import { v4 as uuidv4 } from 'uuid'
+import ytdl from 'ytdl-core'
 
 // ffmpeg.setFfmpegPath(ffmpegInstaller.path)
 // ffmpeg.setFfprobePath(ffprobeInstaller.path)
