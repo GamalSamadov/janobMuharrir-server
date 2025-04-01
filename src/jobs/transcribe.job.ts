ce4fa94fa6b07bcb0480ec76f61b4de9a96f7b40
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-04-02 01:05:10 +0300

    FIX: downloading on production issue 5.2.2

--- src/jobs/transcribe.job.ts
+++ src/jobs/transcribe.job.ts
@@ -50,6 +50,8 @@ export async function runTranscriptionJob(
 
 		logger.info(`Workspaceing video info for: ${url}`)
 		const info = await youtubeDl(url, {
+			userAgent:
+				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
 			dumpSingleJson: true,
 			noWarnings: true,
 			getUrl: true,
@@ -91,6 +93,8 @@ export async function runTranscriptionJob(
 
 		logger.info(`Getting direct audio stream URL for: ${url}`)
 		const audioUrl = await youtubeDl(url, {
+			userAgent:
+				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
 			dumpSingleJson: true,
 			noWarnings: true,
 			getUrl: true,
