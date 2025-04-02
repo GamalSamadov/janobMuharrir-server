5d5ba3961b43eb7d7000227bf8b386ab70822412
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-04-02 04:58:22 +0300

    FIX: download issue 6.4

--- src/jobs/transcribe.job.ts
+++ src/jobs/transcribe.job.ts
@@ -23,19 +23,10 @@ const delay = (ms: number) => new Promise(res => setTimeout(res, ms))
 
 const ytdlOptions = {
 	filter: 'audioonly' as const,
-	quality: 'highestaudio',
-	highWaterMark: 1 << 25,
-	dlChunkSize: 0,
-	retries: 3,
 	requestOptions: {
 		headers: {
 			'User-Agent':
-				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-			Accept: '*/*',
-			'Accept-Language': 'en-US,en;q=0.9',
-			Referer: 'https://www.youtube.com/',
-			Origin: 'https://www.youtube.com',
-			DNT: '1',
+				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
 			cookie: process.env.YOUTUBE_COOKIE || ''
 		}
 	}
